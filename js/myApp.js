81142e050f2cb045c3735e230375bd2f9453aaba
Bragar Oleksandr <bragardlin@gmail.com>
2018-10-18 18:39:35 +0300

    contact in main page

--- js/myApp.js
+++ js/myApp.js
@@ -1,1 +1,23 @@
-
+function required(){
+  var name = document.forms["cont_form"].elements["name"].value;
+  var email = document.forms["cont_form"].elements["email"].value;
+  var phone = document.forms["cont_form"].elements["phone"].value;
+  var message = document.forms["cont_form"].elements["message"].value;
+      if(name == null || name == ""){
+          alert("Введіть ім'я!");
+          return false;
+      }
+      if(email == null || email == ""){
+        alert("Введіть email!");
+        return false;
+    }
+      if(phone == null || phone == ""){
+          alert("Введіть номер телефону.");
+          return false;
+      }
+      if(message == null || message == ""){
+          alert("Введіть повідомлення.");
+          return false;
+      }
+      else{return true;}
+}
