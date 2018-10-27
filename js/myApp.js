6565e06e19e25c2534d25715bb0f85d2acd1a339
Volodymyr <vovikys1997@gmail.com>
2018-10-27 19:49:35 +0300

    kids room background-table

--- js/myApp.js
+++ js/myApp.js
@@ -1,33 +1,49 @@
-function required(){
-  var name = document.forms["cont_form"].elements["name"].value;
-  var email = document.forms["cont_form"].elements["email"].value;
-  var phone = document.forms["cont_form"].elements["phone"].value;
-  var message = document.forms["cont_form"].elements["message"].value;
-      if(name == null || name == ""){
-          alert("Введіть ім'я!");
-          return false;
-      }
-      if(email == null || email == ""){
+function required() {
+    var name = document.forms["cont_form"].elements["name"].value;
+    var email = document.forms["cont_form"].elements["email"].value;
+    var phone = document.forms["cont_form"].elements["phone"].value;
+    var message = document.forms["cont_form"].elements["message"].value;
+    if (name == null || name == "") {
+        alert("Введіть ім'я!");
+        return false;
+    }
+    if (email == null || email == "") {
         alert("Введіть email!");
         return false;
     }
-      if(phone == null || phone == ""){
-          alert("Введіть номер телефону.");
-          return false;
-      }
-      if(message == null || message == ""){
-          alert("Введіть повідомлення.");
-          return false;
-      }
-      else{return true;}
+    if (phone == null || phone == "") {
+        alert("Введіть номер телефону.");
+        return false;
+    }
+    if (message == null || message == "") {
+        alert("Введіть повідомлення.");
+        return false;
+    }
+    else {
+        return true;
+    }
 }
 
 
 /*add footer*/
 const serverURL = 'https://jaroslav-credopro.github.io/Klaptyk/';
-$(document).ready(function(){
+$(document).ready(function () {
 
     $('#footer-for-all-website-id').load(serverURL + 'data/footer.html');
 
-  
-    });
+    /*background table (Vova_Hundyak) */
+
+    $('.special-row').hover(function () {
+            $(this).addClass('bg-table');
+        },
+        function () {
+            $(this).removeClass('bg-table');
+        });
+    $('.special-td').hover(function () {
+            $(this).addClass('table-text');
+        },
+        function () {
+            $(this).removeClass('table-text');
+        });
+
+});
