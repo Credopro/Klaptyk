a532eb7a129b2609c8da0cc16a4eb4a4f6931f21
Jaroslav Credopro <credopro@gmail.com>
2018-10-30 16:01:57 +0200

    about

--- js/myApp.js
+++ js/myApp.js
@@ -1,31 +1,32 @@
 // Start using JQuery
 
-const serverURL = 'https://jaroslav-credopro.github.io/Klaptyk/';
-// let serverURL = 'http://klaptyk/Klaptyk/';
+// const serverURL = 'https://jaroslav-credopro.github.io/Klaptyk/';
+let serverURL = 'http://klaptyk/Klaptyk/';
 $(document).ready(function () {
+	debugger;
 
 	// add footer
 
-	$('#footer-for-all-website-id').load(serverURL + 'footer/footer.html');
 
-	// add header
+
+	// // add header
 	$('#header-for-all-website-id').load(serverURL + 'header/header.html');
 
 	// aboutus
-	$('#aboutus').load(serverURL + 'aboutus/aboutus.html');
+	$('#about').load(serverURL + 'aboutus/about.html');
 	// courses-index
 	// $('#courses-index').load(serverURL + 'coursers/courses_index.html');
 
-
+	$('#footer-for-all-website-id').load(serverURL + 'footer/footer.html');
 	// scroll
-	$(window).scroll(function () {
-		if ($(this).scrollTop() >= 120) {
-			$('nav').addClass(' fixed-top ');
-		}
-		else {
-			$('nav').removeClass(' fixed-top ');
-		}
-	});
+	// $(window).scroll(function () {
+	// 	if ($(this).scrollTop() >= 120) {
+	// 		$('nav').addClass(' fixed-top ');
+	// 	}
+	// 	else {
+	// 		$('nav').removeClass(' fixed-top ');
+	// 	}
+	// });
 	// end header
 
 });
