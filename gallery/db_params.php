69a4cc7b353cad3f626995946f8bda111663081e
Jaroslav Credopro <credopro@gmail.com>
2018-12-06 23:41:01 +0200

    dell

--- gallery/db_params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Массив с параметрами подключения к базе данных
-return array(
-    'host' => 'www.host2.com',
-    'dbname' => 'klaptyk',
-    'user' => 'root',
-    'password' => '', 
-);  
