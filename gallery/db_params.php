59409596af05bfd6034a6ade3e44454edaeee6f2
Jaroslav Credopro <credopro@gmail.com>
2019-09-20 06:33:18 +0300

    deleted json

--- gallery/db_params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Массив с параметрами подключения к базе данных
-return array(
-    'host' => 'localhost',
-    'dbname' => 'c22_site',
-    'user' => 'c22site',
-    'password' => 'YxWB8jj8!',
-);  
